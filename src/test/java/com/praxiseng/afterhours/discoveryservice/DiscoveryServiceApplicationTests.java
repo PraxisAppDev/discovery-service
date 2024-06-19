b3abe9cb1d9df55a4cc7b88eec227418396160d0
Jim Zombek <jimzombek@gmail.com>
2024-06-19 13:57:12 -0400

    initial push

---
+++ src/test/java/com/praxiseng/afterhours/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praxiseng.afterhours.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
